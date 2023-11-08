4539329750da46388a68dd4fc20ec5424e3b6759
이주형 JuhyeongLee <smilebank7@naver.com>
2023-11-08 13:10:48 +0900

    Merge branch 'main' of /Users/inshjh/Documents/GitHub/Kroocrew2 with conflicts.

---
+++ lib/widgets/Top_Appbar.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'Top_Appbar.dart';
+
+// **************************************************************************
+// FunctionalWidgetGenerator
+// **************************************************************************
+
+class TopAppbar extends StatelessWidget {
+  const TopAppbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => topAppbar();
+}
+
+class UnivContainer extends StatelessWidget {
+  const UnivContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => univContainer();
+}
+
+class DepartContainer extends StatelessWidget {
+  const DepartContainer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext _context) => departContainer();
+}
